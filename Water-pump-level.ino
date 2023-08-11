739fd5e800b179995c2f633b16dcaedf1f340a69
Ezequiel Q <ezeaquev@gmail.com>
2023-08-10 23:43:55 -0300

    Usage of OTA for updating code

--- Water-pump-level.ino
+++ Water-pump-level.ino
@@ -1,6 +1,8 @@
 #include "src/ServerManager.h"
 #include "src/WaterPumpManager.h"
 #include "src/WhatsappNotificator.h"
+#include "src/Ota.h"
+
 ServerManager serverManager;
 
 byte echoPin = D5;
@@ -9,7 +11,7 @@ byte relayPin = D7;
 bool alreadySentMessage = false;
 WaterPumpManager *waterPumpManager;
 WhatsappNotificator *wppNotificator;
-
+OtaManager *otaManager;
 void setup()
 {
   Serial.begin(9600);
@@ -25,10 +27,12 @@ void setup()
   // Create whatsappNotificator and waterPumpManager
   wppNotificator = new WhatsappNotificator(ServerManager::wppConfig.get());
   waterPumpManager = new WaterPumpManager(ServerManager::waterPumpConfig.get(), triggerPin, echoPin);
+  otaManager = new OtaManager();
 }
 
 void loop()
 {
+  otaManager->handle();
   waterPumpManager->CalculateWaterLevel();
   if (WaterPumpManager::isRunning && ServerManager::waterPumpConfig->automaticPump)
   {
